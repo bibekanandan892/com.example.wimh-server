870352c2d13940cd78de0b181ffc81aa826067cd
bibekdivine <bibek.divine@gmail.com>
2023-05-08 16:08:06 +0530

    shadow Jar setup

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM openjdk:11-jre-slim
-WORKDIR /app
-COPY build/libs/*.jar app.jar
 EXPOSE 8080
-CMD ["java", "-jar", "app.jar"]
+WORKDIR /app
+COPY build/libs/*.jar /app/wimh-server.jar
+ENTRYPOINT ["java","-jar","/app/wimh-server.jar"]
--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,8 @@ plugins {
     application
     kotlin("jvm") version "1.5.10"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.5.10"
+    id("com.github.johnrengelman.shadow") version "7.1.0"
+
 }
 
 
@@ -23,7 +25,6 @@ group = "com.example"
 version = "0.0.1"
 application {
     mainClass.set("io.ktor.server.netty.EngineMain")
-
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
@@ -53,7 +54,7 @@ dependencies {
     implementation("io.ktor:ktor-client-core:$ktor_version")
     implementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
     implementation("io.ktor:ktor-serialization-gson:$ktor_version")
-    implementation ("io.ktor:ktor-client-apache:$ktor_version")
+    implementation("io.ktor:ktor-client-apache:$ktor_version")
     implementation("io.ktor:ktor-client-logging:$ktor_version")
 
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
@@ -61,17 +62,17 @@ dependencies {
 
 
 
-    implementation ("io.ktor:ktor-client-core:$ktor_version")
-    implementation ("io.ktor:ktor-client-cio:$ktor_version")
-    implementation ("io.ktor:ktor-client-serialization:$ktor_version")
-    implementation ("io.ktor:ktor-client-websockets:$ktor_version")
-    implementation ("io.ktor:ktor-client-logging:$ktor_version")
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+    implementation("io.ktor:ktor-client-serialization:$ktor_version")
+    implementation("io.ktor:ktor-client-websockets:$ktor_version")
+    implementation("io.ktor:ktor-client-logging:$ktor_version")
     implementation("io.ktor:ktor-client-auth:$ktor_version")
-    implementation ("com.google.code.gson:gson:2.10")
+    implementation("com.google.code.gson:gson:2.10")
 
 
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
-    implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.0")
     //endregion
 
     //Region MongoDB
@@ -87,7 +88,7 @@ dependencies {
     implementation("org.apache.commons:commons-email:$apache_commons_version")
 
     implementation("com.google.firebase:firebase-admin:$firebase_admin_version")
-    implementation ("io.ktor:ktor-auth-jwt:1.6.1")
+    implementation("io.ktor:ktor-auth-jwt:1.6.1")
     //region Koin
     // Koin Core features
     implementation("io.insert-koin:koin-ktor:$koin_version")
@@ -99,3 +100,60 @@ dependencies {
 //    testImplementation("io.ktor:ktor-server-tests:$ktor_version")
     //endregion
 }
+
+
+tasks{
+    shadowJar {
+        manifest {
+            attributes(Pair("Main-Class", "com.example.ApplicationKt"))
+        }
+    }
+}
+
+
+//tasks{
+//    fatJar {
+//        archiveFileName.set("com.example.wimh-server-$version-all.jar")
+//    }
+//}
+
+
+
+//tasks {
+//    shadowJar {
+//        manifest {
+//            attributes(Pair("Main-Class", "com.example.ApplicationKt"))
+//        }
+//    }
+//
+//    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+//        kotlinOptions.jvmTarget = "1.8"
+//    }
+//
+//    // Define the fatJar task for ktor
+//    val fatJar = tasks.register<Jar>("fatJar") {
+//        archiveClassifier.set("all")
+//        from(sourceSets.main.get().output)
+//        manifest {
+//            attributes["Main-Class"] = "com.example.ApplicationKt"
+//        }
+//        dependsOn(configurations.runtimeClasspath)
+//        from({
+//            configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+//        })
+//    }
+//    // Configure the ktor plugin to use the fatJar task
+//    configure<io.ktor.gradle.KtorPluginExtension> {
+//        application {
+//            mainClass.set("com.example.ApplicationKt")
+//            executable.set(false)
+//        }
+//        packageApplication {
+//            dependsOn(fatJar)
+//            from(fatJar.get().archiveFile)
+//            archiveFileName.set("com.example.wimh-server-$version-all.jar")
+//        }
+//    }
+//}
+
+
